b9d5eb8f17303916c06dd36fa24491ea31466ab6
Neha0315 <nehapanduri@gmail.com>
2025-02-11 19:01:57 +0000

    before break

---
+++ src/week1/StatusApi/UnitTests1.cs
@@ -0,0 +1,11 @@
+﻿namespace Todos.Tests;
+
+public class UnitTest1
+{
+	[Fact] //Attributes(@Fact())
+
+	public void Test1()
+	{
+		throw new Exception();
+	}
+}
---
+++ src/week1/TodosSolution/Todos.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Todos.Api.Utils;
+
+namespace Todos.Tests;
+
+public class UnitTest1
+{
+    [Fact] // Attributes (@Fact())
+    public void CanFormatAName()
+    {
+        // Given
+        string firstName = "Bob", lastName = "Smith", fullName;
+
+
+        // When
+        fullName = Formatters.FormatName(firstName, lastName);
+
+        // Then 
+        Assert.Equal("Smith, Bob", fullName);
+    }
+
+    [Theory]
+    [InlineData("Bob", "Smith", "Smith, Bob")]
+    [InlineData("Luke", "Skywalker", "Skywalker, Luke")]
+    public void CanFormatAnyName(string firstName, string lastName, string expecting)
+    {
+        var fullName = Formatters.FormatName(firstName, lastName);
+        var thingy = new Formatters();
+
+        //Assert.True(Formatters.IsLongName(fullName));
+        Assert.Equal(expecting, fullName);
+    }
+}
