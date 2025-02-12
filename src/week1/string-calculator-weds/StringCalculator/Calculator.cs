3797c55d2ce1e99cec1dfc777ea2b780f8daa44f
Neha0315 <nehapanduri@gmail.com>
2025-02-12 21:38:44 +0000

    StringCalculator(1-4 done)

---
+++ src/week1/string-calculator-weds/StringCalculator/Calculator.cs
@@ -0,0 +1,48 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        //this is if string is empty
+        if (numbers == "")
+        {
+            return 0;
+        }
+        //this is if string is one value
+        //check if the string is an int, then return result
+        else if (Int32.TryParse(numbers, out int result))
+        {
+            return result;
+        }
+        //everything else
+        else
+        {
+            if (numbers.Contains(","))
+            {
+                //better to separate all the numbers by the commas
+                //add to array of strings
+                string[] values = numbers.Split(',');
+
+                //total sum count
+                int totalSum = 0;
+
+                //for every value, do the same as before
+                //find out if that string is an int and then return that num
+                foreach (string v in values)
+                {
+                    //trim is for if there are spaces, or extra char
+                    if (Int32.TryParse(v.Trim(), out int num))
+                    {
+                        totalSum += num;
+                    }
+                }
+
+                return totalSum;
+
+            }
+
+            return 0;
+
+        }
+    }
+}
---
+++ src/week1/string-calculator-weds/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace StringCalculator;
+public class CalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+
+    [Theory]
+    [InlineData("", 0)]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("1,2", 3)]
+    [InlineData("1,2,3", 6)]
+    [InlineData("1,2,3,4,5,6,7,8,9", 45)]
+    [InlineData("1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ", 45)]
+
+    public void SingleDigit(string value, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(value);
+
+        Assert.Equal(expected, result);
+    }
+}
